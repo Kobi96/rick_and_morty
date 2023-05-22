c6f4cae046809c228f2a86c28ebb3787e1c3cfd0
Kobi96 <tobiasolveira@outlook.com>
2023-05-22 20:38:19 -0300

    updates 22-5-23

--- src/App.js
+++ src/App.js
@@ -1,15 +1,28 @@
 import "./App.css";
 import Cards from "./components/Cards/Cards.jsx";
 import Nav from "./components/Nav/Nav";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import axios from "axios";
-import { Route, Routes } from "react-router-dom";
+import { Route, Routes, useLocation, useNavigate } from "react-router-dom";
 import { ROUTES } from "./helpers/RoutesPath";
 import Detail from "./components/Detail.jsx/Detail";
 import About from "./views/About";
+import Form from "./components/Form/Form";
 
 function App() {
+  const { pathname } = useLocation();
   const [characters, setCharacters] = useState([]);
+  const navigate = useNavigate();
+  const [access, setAccess] = useState(false);
+  const EMAIL = "tobiasolveira@outlook.com";
+  const PASSWORD = "Ogeidojr96!";
+
+  function login(userData) {
+    if (userData.password === PASSWORD && userData.email === EMAIL) {
+      setAccess(true);
+      navigate(ROUTES.HOME);
+    }
+  }
 
   const onSearch = (id) => {
     axios(`https://rickandmortyapi.com/api/character/${id}`).then(
@@ -31,10 +44,15 @@ function App() {
     );
   };
 
+  useEffect(() => {
+    !access && navigate("/");
+  }, [access]);
+
   return (
     <div className="App">
-      <Nav onSearch={onSearch} />
+      {pathname !== "/" && <Nav onSearch={onSearch} />}
       <Routes>
+        <Route path={"/"} element={<Form login={login} />} />
         <Route
           path={ROUTES.HOME}
           element={<Cards characters={characters} onClose={onClose} />}
