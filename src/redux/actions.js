c775083ed38c84e2d0af4c3902c36d05aad929cb
Kobi96 <tobiasolveira@outlook.com>
2023-05-31 23:39:32 -0300

    updates 31-5-23

--- src/redux/actions.js
+++ src/redux/actions.js
@@ -1,4 +1,4 @@
-import { ADD_FAV, REMOVE_FAV } from "./types";
+import { ADD_FAV, FILTER, ORDER, REMOVE_FAV } from "./types";
 
 export const addFav = (character) => {
   return { type: ADD_FAV, payload: character };
@@ -6,4 +6,12 @@ export const addFav = (character) => {
 
 export const removeFav = (id) => {
   return { type: REMOVE_FAV, payload: id };
+};
+
+export const filterCards = (gender) => {
+  return { type: FILTER, payload: gender };
+};
+
+export const orderCards = (orden) => {
+  return { type: ORDER, payload: orden };
 };
--- src/redux/reducer.js
+++ src/redux/reducer.js
@@ -1,7 +1,8 @@
-import { REMOVE_FAV, ADD_FAV } from "./types";
+import { REMOVE_FAV, ADD_FAV, FILTER, ORDER } from "./types";
 
 const initialState = {
   myFavorites: [],
+  allCharacters: [],
 };
 
 const rootReducer = (state = initialState, action) => {
@@ -9,17 +10,47 @@ const rootReducer = (state = initialState, action) => {
 
   switch (type) {
     case ADD_FAV:
+      let copy1 = state.allCharacters;
+      copy1.push(payload);
       return {
         ...state,
-        myFavorites: [...state.myFavorites, payload],
+        myFavorites: copy1,
+        allCharacters: copy1,
       };
     case REMOVE_FAV:
+      let copy2 = state.myFavorites.filter(
+        (character) => character.id !== Number(payload)
+      );
       return {
         ...state,
-        myFavorites: state.myFavorites.filter(
-          (character) => character.id !== Number(payload)
-        ),
+        myFavorites: copy2,
       };
+    case FILTER:
+      let copy3 = state.allCharacters.filter(
+        (character) => character.gender === payload
+      );
+      return {
+        ...state,
+        myFavorites: copy3,
+      };
+
+    case ORDER:
+      let copy4 = state.allCharacters;
+
+      let order = copy4.sort((a, b) => {
+        if (payload === "A") {
+          return a.id - b.id;
+        } else if (payload === "D") {
+          return b.id - a.id;
+        } else {
+          return 0;
+        }
+      });
+      return {
+        ...state,
+        myFavorites: order,
+      };
+
     default:
       return state;
   }
