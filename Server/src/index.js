e2fcb801b8d54ce7e384b9ae75f03d05ed15b8b6
Kobi96 <tobiasolveira@outlook.com>
2023-06-15 23:32:53 -0300

    updates 15-6-23

--- Server/src/index.js
+++ Server/src/index.js
@@ -1,23 +1,12 @@
 const http = require("http");
-const data = require("./utils/data");
-const { stringify } = require("querystring");
+const getCharById = require("./controllers/getCharById");
 
 http
   .createServer((req, res) => {
     res.setHeader("Access-Control-Allow-Origin", "*");
-
-    const { url } = req;
-    if (url.includes("rickandmorty/character/")) {
-      const id = url.split("/").at(-1);
-      const character = data.find((char) => char.id == id);
-
-      if (character) {
-        res.writeHead(200, { "Content-Type": "application/json" });
-        return res.end(JSON.stringify(character));
-      } else {
-        res.writeHead(404, { "Content-Type": "application/json" });
-        return res.end(JSON.stringify({ error: "Character not found" }));
-      }
+    if (req.url.includes("/rickandmorty/character")) {
+      let id = req.url.split("/").at(-1);
+      getCharById(res, id);
     }
   })
-  .listen(3001, "localhost");
+  .listen(3001, () => console.log("Port on 3001"));
