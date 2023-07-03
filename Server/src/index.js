a3b1358d71b7fbc142f774340dbd2fa3fee91bf7
Kobi96 <tobiasolveira@outlook.com>
2023-07-03 17:45:05 -0300

    updates 03-07-23

--- Server/src/index.js
+++ Server/src/index.js
@@ -1,15 +1,5 @@
-const express = require("express");
-const morgan = require("morgan");
-const cors = require("cors");
-const router = require("./routes");
-
+const server = require("./app");
 const PORT = 3001;
-const server = express();
-
-server.use(morgan("dev"));
-server.use(cors());
-server.use(express.json());
-server.use("/rickandmorty", router);
 
 server.listen(PORT, () => {
   console.log("Server raised in port: " + PORT);
