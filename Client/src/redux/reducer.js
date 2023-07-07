be0a85f6b0bd117257c784dafd3edd12515583ba
Kobi96 <tobiasolveira@outlook.com>
2023-07-06 22:12:48 -0300

    resolution favorites

--- Client/src/redux/reducer.js
+++ Client/src/redux/reducer.js
@@ -11,20 +11,20 @@ const rootReducer = (state = initialState, { type, payload }) => {
       return { ...state, myFavorites: payload, allCharacters: payload };
 
     case FILTER:
-      let copy3 = state.allCharacters.filter((char) => {
+      let copy1 = state.allCharacters.filter((char) => {
         return char.gender === payload;
       });
       return {
         ...state,
-        myFavorites: copy3,
+        myFavorites: copy1,
       };
     case ORDER:
-      let copy4 = state.allCharacters.sort((a, b) => {
+      let copy2 = state.allCharacters.sort((a, b) => {
         return payload === "A" ? a.id - b.id : b.id - a.id;
       });
       return {
         ...state,
-        myFavorites: copy4,
+        myFavorites: copy2,
       };
     case REMOVE_FAV:
       return { ...state, myFavorites: payload };
