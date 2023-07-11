76ed12f9e315373594b8e71515fcc61cf92a62a3
Kobi96 <tobiasolveira@outlook.com>
2023-07-11 10:54:29 -0300

    ormPart2 - ok

--- Server/src/routes/index.js
+++ Server/src/routes/index.js
@@ -1,13 +1,14 @@
-const { Router } = require("express");
+const router = require("express").Router();
 const getCharacterById = require("../controllers/getCharById");
-const { postFav, deleteFav } = require("../controllers/handleFavorites");
+const postFav = require("../controllers/postFav");
+const deleteFav = require("../controllers/deleteFav");
+const postUser = require("../controllers/postUser");
 const login = require("../controllers/login");
 
-const router = Router();
-
-router.get("/character/:id", (req, res) => getCharacterById(req, res));
-router.get("/login", (req, res) => login(req, res));
-router.post("/fav", (req, res) => postFav(req, res));
-router.delete("/fav/:id", (req, res) => deleteFav(req, res));
+router.get("/character/:id", getCharacterById);
+router.get("/login", login);
+router.post("/login", postUser);
+router.post("/fav", postFav);
+router.delete("/fav/:id", deleteFav);
 
 module.exports = router;
